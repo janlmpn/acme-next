437e782afa6b0415c4205feb90c74af4363b8054
John Limpin <john.limpin@gmail.com>
2025-01-06 23:46:16 +0800

    Lint error fixes

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -30,7 +30,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
-  const [_, formAction] = useActionState(deleteInvoiceWithId, undefined);
+  const [,formAction] = useActionState(deleteInvoiceWithId, undefined);
   
   return (
     <>
